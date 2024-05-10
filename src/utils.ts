78cdbc6b23601d6a00f0223076a0d3ae40c8e885
Kiselev Andrey <omega-faworit@yandex.ru>
2024-05-10 21:11:40 +0300

    use more performant comparison

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,6 @@
 export const areMapsEqual = (m1: Map<any, any>, m2: Map<any, any>) => {
   return (
     m1.size === m2.size &&
-    Array.from(m1.entries()).every(
-      ([k1, v1]) => m2.has(k1) && v1 === m2.get(k1)
-    )
+    [...m1.entries()].every(([k1, v1]) => m2.has(k1) && v1 === m2.get(k1))
   );
 };
